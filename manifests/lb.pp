201bd1755ac17af390a270a7d4621be73de26af0
Shaun Marshall <shaun.marshall@enstratius.com>
2015-06-24 11:52:24 +0100

    added stats to HAProxy, added restart after config changes

--- manifests/lb.pp
+++ manifests/lb.pp
@@ -3,9 +3,30 @@ class stack_wordpress::lb {
 
   class { 'haproxy': }
 
+  haproxy::listen { 'stats':
+    ports     => '80',
+    options   => {
+      'mode'  => 'http',
+      'stats' => [
+      'enable','hide-version','realm Haproxy\ Statistics',
+      'uri /stats','auth admin:Password12'
+
+    ],
+  },
+}
+
   haproxy::listen { 'stack00':
     collect_exported => true,
+    mode              => 'http',
     ipaddress        => '0.0.0.0',
     ports            => '80',
+    options   => {
+    'option'  => [
+      'tcplog','httpclose','forwardfor'],
+  }
+ }
+
+  exec { "restart_haproxy":
+    command => "/etc/init.d/haproxy restart"
   }
 }
